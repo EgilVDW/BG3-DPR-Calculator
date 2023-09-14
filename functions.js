4e081254d22ab54a531cb4c457eca29bd7a1eadd
Morten Buch <mobu26304@edu.ucl.dk>
2023-09-14 18:52:46 +0200

    functions.js

--- functions.js
+++ functions.js
@@ -1,69 +1,90 @@
-let accuracyType = document.getElementById("accuracyType").value
-let targetAC = parseInt(document.getElementById("targetAC").value);
-let attackBonus = parseInt(document.getElementById("attackBonus").value);
-let critMin = parseInt(document.getElementById("critMin").value);
-let attacksPerTurn = parseInt(document.getElementById("attacksPerTurn").value);
-let hitChance = ( 21 + attackBonus - targetAC ) / 20;
-let critChance = ( 21 - critMin ) / 20;
-let hitChancePct = ( hitChance - critChance ) * 100;
-let critChancePct = critChance * 100;
-let missChancePct = ( 1 - hitChance ) * 100;
-let damageRoll = parseInt(document.getElementById("damageRoll").value);
-let damageBonus = parseInt(document.getElementById("damageBonus").value);
-let damageBonusCrit = parseInt(document.getElementById("damageBonusCrit").value);
-let damageBonusCritMul = parseInt(document.getElementById("damageBonusCritMul").value) * 2;
-let damageOnMiss = parseInt(document.getElementById("damageOnMiss").value);
-let damageAvg = ( hitChance * ( damageRoll + damageBonus ) + critChance * ( damageRoll + damageBonusCrit + damageBonusCritMul ) + damageOnMiss * ( 1 - hitChance )) * attacksPerTurn;
+let accuracyType;
+let targetAC;
+let attackBonus;
+let critMin;
+let attacksPerTurn;
+let hitChance;
+let critChance;
+let hitChancePct;
+let critChancePct;
+let missChancePct;
+let damageRoll;
+let damageBonus;
+let damageBonusCrit;
+let damageBonusCritMul;
+let damageOnMiss;
+let damageAvg;
+
+
+accuracyType = document.getElementById("accuracyType"+counter).value;
+targetAC = parseInt(document.getElementById("targetAC"+counter).value);
+attackBonus = parseInt(document.getElementById("attackBonus"+counter).value);
+critMin = parseInt(document.getElementById("critMin"+counter).value);
+attacksPerTurn = parseInt(document.getElementById("attacksPerTurn"+counter).value);
+hitChance = ( 21 + attackBonus - targetAC ) / 20;
+critChance = ( 21 - critMin ) / 20;
+hitChancePct = ( hitChance - critChance ) * 100;
+critChancePct = critChance * 100;
+missChancePct = ( 1 - hitChance ) * 100;
+damageRoll = parseInt(document.getElementById("damageRoll"+counter).value);
+damageBonus = parseInt(document.getElementById("damageBonus"+counter).value);
+damageBonusCrit = parseInt(document.getElementById("damageBonusCrit"+counter).value);
+damageBonusCritMul = parseInt(document.getElementById("damageBonusCritMul"+counter).value) * 2;
+damageOnMiss = parseInt(document.getElementById("damageOnMiss"+counter).value);
+damageAvg = ( hitChance * ( damageRoll + damageBonus ) + critChance * ( damageRoll + damageBonusCrit + damageBonusCritMul ) + damageOnMiss * ( 1 - hitChance )) * attacksPerTurn;
 
 // event listener for accuracy type
-document.getElementById("accuracyType").addEventListener("change", () => {
-    hitChanceOutput("hitChanceOutput"),
-    critChanceOutput("critChanceOutput"),
-    hitChancePctOutput("hitChancePctOutput"),
-    critChancePctOutput("critChancePctOutput"),
-    missChancePctOutput("missChancePctOutput"),
-    damageAvgOutput("damageAvgOutput")
-});
-
-// event listener for target AC
-document.getElementById("targetAC").addEventListener("input", () => {
-    hitChanceOutput("hitChanceOutput"),
-    hitChancePctOutput("hitChancePctOutput"),
-    missChancePctOutput("missChancePctOutput"),
-    damageAvgOutput("damageAvgOutput")
-});
-
-// event listener for attack bonus
-document.getElementById("attackBonus").addEventListener("input", () => {
-    hitChanceOutput("hitChanceOutput"),
-    hitChancePctOutput("hitChancePctOutput"),
-    missChancePctOutput("missChancePctOutput"),
-    damageAvgOutput("damageAvgOutput")
-});
-
-// event listener for min. to crit
-document.getElementById("critMin").addEventListener("input", () => {
-    hitChanceOutput("hitChanceOutput"),
-    critChanceOutput("critChanceOutput"),
-    hitChancePctOutput("hitChancePctOutput"),
-    critChancePctOutput("critChancePctOutput"),
-    missChancePctOutput("missChancePctOutput"),
-    damageAvgOutput("damageAvgOutput")
-});
-
-// event listeners for damage per turn
-document.getElementById("attacksPerTurn").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
-document.getElementById("damageRoll").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
-document.getElementById("damageBonus").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
-document.getElementById("damageBonusCrit").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
-document.getElementById("damageBonusCritMul").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
-document.getElementById("damageOnMiss").addEventListener("input", () => { damageAvgOutput("damageAvgOutput") });
+function allEventListeners(){
+    document.getElementById("accuracyType"+counter).addEventListener("change", () => {
+        hitChanceOutput("hitChanceOutput"+counter),
+        critChanceOutput("critChanceOutput"+counter),
+        hitChancePctOutput("hitChancePctOutput"+counter),
+        critChancePctOutput("critChancePctOutput"+counter),
+        missChancePctOutput("missChancePctOutput"+counter),
+        damageAvgOutput("damageAvgOutput"+counter)
+    });
+    
+    // event listener for target AC
+    document.getElementById("targetAC"+counter).addEventListener("input", () => {
+        hitChanceOutput("hitChanceOutput"+counter),
+        hitChancePctOutput("hitChancePctOutput"+counter),
+        missChancePctOutput("missChancePctOutput"+counter),
+        damageAvgOutput("damageAvgOutput"+counter)
+    });
+    
+    // event listener for attack bonus
+    document.getElementById("attackBonus"+counter).addEventListener("input", () => {
+        hitChanceOutput("hitChanceOutput"+counter),
+        hitChancePctOutput("hitChancePctOutput"+counter),
+        missChancePctOutput("missChancePctOutput"+counter),
+        damageAvgOutput("damageAvgOutput"+counter)
+    });
+    
+    // event listener for min. to crit
+    document.getElementById("critMin"+counter).addEventListener("input", () => {
+        hitChanceOutput("hitChanceOutput"+counter),
+        critChanceOutput("critChanceOutput"+counter),
+        hitChancePctOutput("hitChancePctOutput"+counter),
+        critChancePctOutput("critChancePctOutput"+counter),
+        missChancePctOutput("missChancePctOutput"+counter),
+        damageAvgOutput("damageAvgOutput"+counter)
+    });
+    
+    // event listeners for damage per turn
+    document.getElementById("attacksPerTurn"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+    document.getElementById("damageRoll"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+    document.getElementById("damageBonus"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+    document.getElementById("damageBonusCrit"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+    document.getElementById("damageBonusCritMul"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+    document.getElementById("damageOnMiss"+counter).addEventListener("input", () => { damageAvgOutput("damageAvgOutput"+counter) });
+}
+allEventListeners();
 
 // calculate chance to hit (includes crit chance)
 function hitChanceOutput(outputID) {
-    let accuracyType = document.getElementById("accuracyType").value
-    let targetAC = parseInt(document.getElementById("targetAC").value);
-    let attackBonus = parseInt(document.getElementById("attackBonus").value);
+    let accuracyType = document.getElementById("accuracyType"+counter).value
+    let targetAC = parseInt(document.getElementById("targetAC"+counter).value);
+    let attackBonus = parseInt(document.getElementById("attackBonus"+counter).value);
     // chance to hit with advantage (roll two dice instead of one and choose the highest number)
     if ( accuracyType == "advantage" ) {
         hitChance = 1 - (( targetAC - attackBonus - 1 ) * ( targetAC - attackBonus - 1 )) / 400;
@@ -107,8 +128,8 @@ function hitChanceOutput(outputID) {
 
 // calculate chance to crit
 function critChanceOutput(outputID) {
-    let accuracyType = document.getElementById("accuracyType").value;
-    let critMin = parseInt(document.getElementById("critMin").value);
+    let accuracyType = document.getElementById("accuracyType"+counter).value;
+    let critMin = parseInt(document.getElementById("critMin"+counter).value);
     // chance to crit with advantage
     if ( accuracyType == "advantage" ) {
         critChance = 1 - ( 1 - ( 21 - critMin ) / 20 ) * ( 1 - ( 21 - critMin ) / 20 );
@@ -131,8 +152,8 @@ function critChanceOutput(outputID) {
 
 // calculate chance to hit as a percentage (excludes crit chance)
 function hitChancePctOutput(outputID) {
-    let hitChance = parseFloat(document.getElementById("hitChanceOutput").value);
-    let critChance = parseFloat(document.getElementById("critChanceOutput").value);
+    let hitChance = parseFloat(document.getElementById("hitChanceOutput"+counter).value);
+    let critChance = parseFloat(document.getElementById("critChanceOutput"+counter).value);
     hitChancePct = ( hitChance - critChance ) * 100;
     // output
     document.getElementById(outputID).value = hitChancePct.toFixed(0);
@@ -140,7 +161,7 @@ function hitChancePctOutput(outputID) {
 
 // calculate chance to crit as a percentage
 function critChancePctOutput(outputID) {
-    let critChance = parseFloat(document.getElementById("critChanceOutput").value);
+    let critChance = parseFloat(document.getElementById("critChanceOutput"+counter).value);
     critChancePct = critChance * 100;
     // output
     document.getElementById(outputID).value = critChancePct.toFixed(0);
@@ -148,7 +169,7 @@ function critChancePctOutput(outputID) {
 
 // calculate chance to miss as a percentage
 function missChancePctOutput(outputID) {
-    let hitChance = parseFloat(document.getElementById("hitChanceOutput").value);
+    let hitChance = parseFloat(document.getElementById("hitChanceOutput"+counter).value);
     missChancePct = ( 1 - hitChance ) * 100;
     // output
     document.getElementById(outputID).value = missChancePct.toFixed(0);
@@ -156,20 +177,30 @@ function missChancePctOutput(outputID) {
 
 // calculate damage per turn
 function damageAvgOutput(outputID) {
-    let attacksPerTurn = parseInt(document.getElementById("attacksPerTurn").value);
-    let damageRoll = parseInt(document.getElementById("damageRoll").value);
-    let damageBonus = parseInt(document.getElementById("damageBonus").value);
-    let damageBonusCrit = parseInt(document.getElementById("damageBonusCrit").value);
-    let damageBonusCritMul = parseInt(document.getElementById("damageBonusCritMul").value);
-    let damageOnMiss = parseInt(document.getElementById("damageOnMiss").value);
+    let attacksPerTurn = parseInt(document.getElementById("attacksPerTurn"+counter).value);
+    let damageRoll = parseInt(document.getElementById("damageRoll"+counter).value);
+    let damageBonus = parseInt(document.getElementById("damageBonus"+counter).value);
+    let damageBonusCrit = parseInt(document.getElementById("damageBonusCrit"+counter).value);
+    let damageBonusCritMul = parseInt(document.getElementById("damageBonusCritMul"+counter).value);
+    let damageOnMiss = parseInt(document.getElementById("damageOnMiss"+counter).value);
     let damageAvg = ( hitChance * ( damageRoll + damageBonus ) + critChance * ( damageRoll + damageBonusCrit + damageBonusCritMul ) + damageOnMiss * ( 1 - hitChance )) * attacksPerTurn;
     // output
-    document.getElementById(outputID).value = damageAvg.toFixed(2);
+    if(!isNaN(damageAvg.toFixed(2))){
+        // document.getElementById(outputID).textContent = damageAvg.toFixed(2) / attacksPerTurn + " x " + attacksPerTurn;
+        document.getElementById(outputID).textContent = damageAvg.toFixed(2);
+
+    }else{
+        document.getElementById(outputID).textContent = 0;
+    }
+
 }
 
-document.getElementById("hitChanceOutput").value = hitChance;
-document.getElementById("critChanceOutput").value = critChance;
-document.getElementById("hitChancePctOutput").value = hitChancePct;
-document.getElementById("critChancePctOutput").value = critChancePct;
-document.getElementById("missChancePctOutput").value = missChancePct;
-document.getElementById("damageAvgOutput").value = damageAvg;
+document.getElementById("hitChanceOutput"+counter).value = hitChance;
+document.getElementById("critChanceOutput"+counter).value = critChance;
+document.getElementById("hitChancePctOutput"+counter).value = hitChancePct;
+document.getElementById("critChancePctOutput"+counter).value = critChancePct;
+document.getElementById("missChancePctOutput"+counter).value = missChancePct;
+document.getElementById("damageAvgOutput"+counter).value = damageAvg;
+
+
+
